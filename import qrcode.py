77e6c8eaebe3ed4878a130912fee217add5a0fc5
sharadabirajdar003 <sharadabirajdar7878@gmail.com>
2026-01-11 14:02:14 +0530

    Update import qrcode.py

--- import qrcode.py
+++ import qrcode.py
@@ -1,6 +1,5 @@
 import qrcode
 
-# Paste your cover letter PDF link here
 pdf_link = "file:///C:/Users/Admin/OneDrive/Desktop/QR%20Code/Sharda_Birajdar_Cover-letter.pdf"
 
 qr = qrcode.QRCode(
@@ -19,3 +18,4 @@ img = qr.make_image(fill_color="black", back_color="white")
 img.save("cover_letter_qr.png")
 
 print("QR Code for Cover Letter PDF created successfully!")
+
